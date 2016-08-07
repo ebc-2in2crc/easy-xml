2e8525b8abdb91c79ad812686af7aee59b93f526
ebc-2in2crc <y015h1n80@gmail.com>
2016-08-07 22:46:07 +0900

    add: chapter-8.4

---
+++ chapter-8/Sample5.java
@@ -0,0 +1,29 @@
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+import org.w3c.dom.Node;
+
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import java.io.FileInputStream;
+
+public class Sample5 {
+    public static void main(String[] args) throws Exception {
+        final DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
+        final DocumentBuilder builder = factory.newDocumentBuilder();
+        final Document doc = builder.parse(new FileInputStream("sample.xml"));
+
+        final Element root = doc.getDocumentElement();
+        walk(root);
+    }
+
+    private static void walk(Node node) {
+        for (Node childNode = node.getFirstChild();
+             childNode != null;
+             childNode = childNode.getNextSibling()) {
+            if (childNode.getNodeType() == Node.ELEMENT_NODE) {
+                System.out.println(childNode.getNodeName());
+                walk(childNode);
+            }
+        }
+    }
+}
---
+++ chapter-8/Sample6.java
@@ -0,0 +1,32 @@
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+import org.w3c.dom.Node;
+
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import java.io.FileInputStream;
+
+public class Sample6 {
+    public static void main(String[] args) throws Exception {
+        final DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
+        final DocumentBuilder builder = factory.newDocumentBuilder();
+        final Document doc = builder.parse(new FileInputStream("sample.xml"));
+
+        final Element root = doc.getDocumentElement();
+        walk(root);
+    }
+
+    private static void walk(Node node) {
+        for (Node childNode = node.getFirstChild();
+             childNode != null;
+             childNode = childNode.getNextSibling()) {
+            if (childNode.getNodeType() == Node.ELEMENT_NODE) {
+                System.out.println(childNode.getNodeName());
+                walk(childNode);
+            } else if (childNode.getNodeType() == Node.TEXT_NODE &&
+                    childNode.getNodeValue().trim().length() != 0) {
+                System.out.println(childNode.getNodeValue());
+            }
+        }
+    }
+}
